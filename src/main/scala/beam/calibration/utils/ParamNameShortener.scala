ebef580cd86fccb461ae35a476800d3007aba824
Muhammad Asif <muhammad.asif1.northstar@gmail.com>
2018-10-10 22:00:20 +0500

    ma/#692-create-sigopt-calibration-folder-for-beamville Updated the function such that it shortens the name until it is less than 100 characters only

--- src/main/scala/beam/calibration/utils/ParamNameShortener.scala
+++ src/main/scala/beam/calibration/utils/ParamNameShortener.scala
@@ -1,27 +1,54 @@
 package beam.calibration.utils
+import scala.util.control.Breaks._
 
 object ParamNameShortener extends App {
 
   var abbrevToParamsMap: Map[String, String] = Map()
 
   def shortenName(paramName: String) = {
-    val paramParts: Array[String] = paramName.split("""\.""")
 
-    var paramParts2: Array[String] = Array()
-    for(i <- 0 until paramParts.length - 1){
+    if(paramName.size > 100) {
+      val paramParts: Array[String] = paramName.split("""\.""")
 
-      val pp = paramParts(i)
-      paramParts2 = paramParts2 :+ pp.take(1)
+      var finalString: String = ""
+      var firstPart: String = ""
+      var restOfString: String = paramName
+
+      breakable {
+        for (i <- 0 until paramParts.length - 1) {
+
+          val pp = paramParts(i)
+          val firstChar = pp.take(1)
+
+          if(firstPart.size > 0){
+            firstPart = firstPart + "." + firstChar
+          }else{
+            firstPart = firstChar
+          }
+
+          restOfString = restOfString.substring(restOfString.indexOf(".") + 1)
+
+          finalString = firstPart + "." + restOfString
+
+          if (finalString.size <= 100) break
+
+        }
+      }
+
+      abbrevToParamsMap = abbrevToParamsMap + (finalString -> paramName)
+    }else{
+      abbrevToParamsMap = abbrevToParamsMap + (paramName -> paramName)
     }
-
-    paramParts2 = paramParts2 :+ paramParts(paramParts.length - 1)
-    val shortName = paramParts2.mkString(".")
-
-    abbrevToParamsMap = abbrevToParamsMap + (shortName -> paramName)
   }
 
   def expandName(shortName: String): String ={
 
     abbrevToParamsMap(shortName)
+  }
+
+  def logParamsMap() = {
+    abbrevToParamsMap.foreach{
+      case(k, v) => println(k + ", " + v)
+    }
   }
 }
