cf0d8e2c07db63bc860897303d5d74a60f8b79d5
David Arias <fdariasm@gmail.com>
2018-10-24 18:01:53 -0500

    #701 - updated gtfs data for bus stops/trips

--- src/main/scala/beam/utils/BeamvilleGtfsUtil.scala
+++ src/main/scala/beam/utils/BeamvilleGtfsUtil.scala
@@ -17,7 +17,8 @@ object BeamvilleGtfsUtil extends App{
     (r, stopIds) <- routesWithStops
     (d, _) <- direction
   } yield {
-    (s"$r-$d", stopIds)
+    val stops = if("WEST".equals(d)) stopIds.reverse else stopIds
+    (s"$r-$d", stops)
   }
 
   def generateTrips(tripStops: Int = 5,
@@ -25,7 +26,7 @@ object BeamvilleGtfsUtil extends App{
                     stopTime: Duration = Duration.ofMinutes(2),
                     travelTime: Duration = Duration.ofMinutes(1).plus(Duration.ofSeconds(30)),
                     startingTime: LocalTime = LocalTime.of(5, 58),
-                    endingTime: LocalTime = LocalTime.of(20, 58)
+                    endingTime: LocalTime = LocalTime.of(18, 58)
                    ): List[List[String]] = {
 
     val totalTripTime: Duration =  stopTime.multipliedBy(tripStops).plus(travelTime.multipliedBy(travels))
@@ -75,70 +76,16 @@ object BeamvilleGtfsUtil extends App{
     }.flatten
   }
 
-  def generateGtfsData() = {
+  val trips = generateTrips()
+  val stopTimes = generateTripStops(trips)
 
-    val routes = List("B1", "B2", "B3")
-    val services = List("TR-2017-ALL-YEAR")
-    val direction = List(("EAST", "0"), ("WEST", "1"))
-    val routesWithStops = List(("B1", 1 to 5), ("B2", 6 to 10), ("B3", 11 to 15))
+  val tripsToPrint = trips.map(_.tail.mkString(",")).mkString("\n")
+  val stopTimesToPrint = stopTimes.mkString("\n")
 
-    val routesDirWithStop = for{
-      (r, stopIds) <- routesWithStops
-      (d, _) <- direction
-    } yield {
-      (s"$r-$d", stopIds)
-    }
-
-    val tripStops = 5
-    val stopTime = Duration.ofMinutes(2)
-    val travels = 4
-    val travelTime = Duration.ofMinutes(1).plus(Duration.ofSeconds(30))
-
-    val totalTripTime: Duration =  stopTime.multipliedBy(tripStops).plus(travelTime.multipliedBy(travels))
-    val adjustedTotalTripTime: Duration =  stopTime.multipliedBy(tripStops - 1).plus(travelTime.multipliedBy(travels))
-
-    val startingTime = LocalTime.of(5, 58)
-    val endingTime = LocalTime.of(20, 58)
-
-    val totalMinutes = ChronoUnit.MINUTES.between(startingTime, endingTime)
-    val totalTrips = totalMinutes / totalTripTime.toMinutes //64
-
-
-    val trips = for{
-      r <- routes
-      s <- services
-      tripId <- 1 to totalTrips.intValue()
-      (d, did) <- direction
-    } yield {
-      List(s"$r-$d", r, s, s"$r-$d-$tripId", "", "", did, "", "", "0", "0")
-    }
-
-    val tripsGroupedByRoute = trips.groupBy(x => x.head)
-
-    val stopTimes = routesDirWithStop.map{ case (routeWithDir, stopIds) =>
-      val tripsForRoute = tripsGroupedByRoute.get(routeWithDir).get
-      var arrivalTime = startingTime
-      var departureTime = arrivalTime.plus(stopTime)
-      for{
-        trip <- tripsForRoute
-        (stopId, index) <- stopIds.zipWithIndex
-      } yield{
-        val r = List(
-          trip(3),
-          arrivalTime.format(formatter),
-          departureTime.format(formatter),
-          stopId.toString,
-          (index + 1).toString,
-          "0",
-          "0"
-        ).mkString(",")
-        arrivalTime = departureTime.plus(travelTime)
-        departureTime = arrivalTime.plus(stopTime)
-        r
-      }
-    }.flatten
-  }
-
-  generateGtfsData()
+  println("Trips")
+  println(tripsToPrint)
+  println("----------------")
+  println("StopTimes")
+  println(stopTimesToPrint)
 
 }
--- test/input/beamville/r5/osm.mapdb.p
+++ test/input/beamville/r5/osm.mapdb.p
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b03286b61719625efd88c145bd0625f0d6530e051cd03e437b1201f732a6b4a
+oid sha256:9b2acad9a8cd0c39a9b908fa78a2d5b86d7073ebcc5fa223cdd88f5f2bc33423
 size 1048576
